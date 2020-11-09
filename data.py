b6df6f40100edcab9f8df096afbba70b10eb152c
ruanyi <iynaur87@sina.com>
2020-11-09 16:24:37 +0800

    fft

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft,ifft
 
 file1 = open('top.txt', 'r') 
 allines = file1.readlines() 
   
 lines = []
-step = 50
+step = 1
 for i in range(len(allines) // step):
     j = step * i
     if j != 0:
@@ -45,7 +47,7 @@ def pick(s_str, e_str, i):
     return v, t
 
 allshow = 0
-if 1: 
+if 0: 
     for i in range(6):
         plt.title(titles[i])
         
@@ -54,17 +56,40 @@ if 1:
         plt.title(titles[i])
         plt.plot(data[i])
 
-        v, t = pick("14:58:00", "15:02:00", i)
+        v, t = pick("15:35:30", "15:36:30", i)
         plt.plot(t, v)
 
+        curtime = ""
         for idx in range(len(data[i])):
-            if idx % 1000 == 0:
+            if timestamp[idx] != curtime and timecnt(timestamp[idx]) % 600 == 0:
                 plt.text(idx, data[i][idx], timestamp[idx])
+                curtime = timestamp[idx]
         mng = plt.get_current_fig_manager()
         mng.window.state('zoomed') 
 
 
         plt.show()
+else:
+    start = 400500
+    length = 1000
+    y = data[0][start:start+ length]
+    yy=fft(y)                     #快速傅里叶变换
+    yreal = yy.real               # 获取实数部分
+    yimag = yy.imag               # 获取虚数部分
+
+    yf=abs(fft(y))                # 取绝对值
+    yf1=abs(fft(y))/len(y)           #归一化处理
+    yf2 = yf1[range(int(len(y)/2))]  #由于对称性，只取一半区间
+
+    xf = np.arange(len(y))        # 频率
+    xf1 = xf
+    xf2 = xf[range(int(len(y)/2))]  #取一半区间
+    plt.plot(xf,yf,'r')
+    mng = plt.get_current_fig_manager()
+    mng.window.state('zoomed') 
+
+
+    plt.show()
 
 tot = [data[0][i]**2 + data[1][i]**2 + data[2][i]**2 for i in range(len(data[0]))]
 plt.plot(tot)
